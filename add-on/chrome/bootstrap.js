f108448d16d12bae41d49a760243e6e48a17714f
Manuel Casas Barrado <manuel.casasbarrado@gmail.com>
2015-12-21 09:45:16 +0100

    Bug 1230946 - Tab sharing is unpaused when switching tabs. r=mikedeboer

--- add-on/chrome/bootstrap.js
+++ add-on/chrome/bootstrap.js
@@ -485,6 +485,7 @@ var WindowListener = {
           // Watch for title changes as opposed to location changes as more
           // metadata about the page is available when this event fires.
           gBrowser.addEventListener("DOMTitleChanged", this);
+          this._browserSharePaused = false;
         }
 
         this._maybeShowBrowserSharingInfoBar();
@@ -506,6 +507,7 @@ var WindowListener = {
         gBrowser.tabContainer.removeEventListener("TabSelect", this);
         gBrowser.removeEventListener("DOMTitleChanged", this);
         this._listeningToTabSelect = false;
+        this._browserSharePaused = false;
       },
 
       /**
@@ -537,26 +539,37 @@ var WindowListener = {
         }
 
         let box = gBrowser.getNotificationBox();
-        let paused = false;
+        let pauseButtonLabel = this._getString(this._browserSharePaused ?
+                                               "infobar_button_resume_label" :
+                                               "infobar_button_pause_label");
+        let pauseButtonAccessKey = this._getString(this._browserSharePaused ?
+                                                   "infobar_button_resume_accesskey" :
+                                                   "infobar_button_pause_accesskey");
+        let barLabel = this._getString(this._browserSharePaused ?
+                                       "infobar_screenshare_paused_browser_message" :
+                                       "infobar_screenshare_browser_message2");
         let bar = box.appendNotification(
-          this._getString("infobar_screenshare_browser_message2"),
+          barLabel,
           kBrowserSharingNotificationId,
           // Icon is defined in browser theme CSS.
           null,
           box.PRIORITY_WARNING_LOW,
           [{
-            label: this._getString("infobar_button_pause_label"),
-            accessKey: this._getString("infobar_button_pause_accesskey"),
+            label: pauseButtonLabel,
+            accessKey: pauseButtonAccessKey,
             isDefault: false,
             callback: (event, buttonInfo, buttonNode) => {
-              paused = !paused;
-              bar.label = paused ? this._getString("infobar_screenshare_paused_browser_message") :
-                this._getString("infobar_screenshare_browser_message2");
-              bar.classList.toggle("paused", paused);
-              buttonNode.label = paused ? this._getString("infobar_button_resume_label") :
-                this._getString("infobar_button_pause_label");
-              buttonNode.accessKey = paused ? this._getString("infobar_button_resume_accesskey") :
-                this._getString("infobar_button_pause_accesskey");
+              this._browserSharePaused = !this._browserSharePaused;
+              bar.label = this._getString(this._browserSharePaused ?
+                                          "infobar_screenshare_paused_browser_message" :
+                                          "infobar_screenshare_browser_message2");
+              bar.classList.toggle("paused", this._browserSharePaused);
+              buttonNode.label = this._getString(this._browserSharePaused ?
+                                                 "infobar_button_resume_label" :
+                                                 "infobar_button_pause_label");
+              buttonNode.accessKey = this._getString(this._browserSharePaused ?
+                                                     "infobar_button_resume_accesskey" :
+                                                     "infobar_button_pause_accesskey");
               return true;
             }
           },
@@ -570,6 +583,9 @@ var WindowListener = {
             }
           }]
         );
+
+        // Sets 'paused' class if needed.
+        bar.classList.toggle("paused", !!this._browserSharePaused);
 
         // Keep showing the notification bar until the user explicitly closes it.
         bar.persistence = -1;

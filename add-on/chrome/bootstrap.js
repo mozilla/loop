45d3299328516bc18a21f77c508807f4f27ed612
mikedeboer <info@mikedeboer.nl>
2016-02-04 17:21:52 +0100

    Bug 1154277 - follow-up: hook panel close to content window.close again. r=Standard8

--- add-on/chrome/bootstrap.js
+++ add-on/chrome/bootstrap.js
@@ -134,6 +134,10 @@ var WindowListener = {
         return new Promise((resolve) => {
           let callback = iframe => {
             let mm = iframe.QueryInterface(Ci.nsIFrameLoaderOwner).frameLoader.messageManager;
+            if (!("messageManager" in iframe)) {
+              iframe.messageManager = mm;
+            }
+            this.hookWindowCloseForPanelClose(iframe);
 
             mm.sendAsyncMessage("Social:WaitForDocumentVisible");
             mm.addMessageListener("Social:DocumentVisible", () => resolve(mm));

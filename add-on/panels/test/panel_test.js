5067760e5381db154125446d4ae3b9ff2105b407
Mark Banner <standard8@mozilla.com>
2016-04-05 12:34:42 +0100

    Bug 1261118 - Fix regression where the Turn notifications on/off button doesn't update its state. r=mancas

--- add-on/panels/test/panel_test.js
+++ add-on/panels/test/panel_test.js
@@ -221,7 +221,9 @@ describe("loop.panel", function() {
       },
       locale: "en-US"
     });
-    sandbox.stub(document.mozL10n, "get").returns("Fake title");
+    sandbox.stub(document.mozL10n, "get", function(stringName) {
+      return stringName;
+    });
   });
 
   afterEach(function() {
@@ -491,6 +493,18 @@ describe("loop.panel", function() {
           view = mountTestComponent();
         });
 
+        it("should show a message to turn notifications off when they are on", function() {
+          LoopMochaUtils.stubLoopRequest({
+            GetDoNotDisturb: function() { return false; }
+          });
+
+          view.showDropdownMenu();
+
+          var menuitem = view.getDOMNode().querySelector(".entry-settings-notifications");
+
+          expect(menuitem.textContent).eql("settings_menu_item_turnnotificationsoff");
+        });
+
         it("should toggle mozLoop.doNotDisturb to false", function() {
           var stub = sinon.stub();
           LoopMochaUtils.stubLoopRequest({
@@ -503,6 +517,18 @@ describe("loop.panel", function() {
 
           sinon.assert.calledOnce(stub);
           sinon.assert.calledWithExactly(stub, false);
+        });
+
+        it("should show a message to turn notifications on when they are off", function() {
+          LoopMochaUtils.stubLoopRequest({
+            GetDoNotDisturb: function() { return true; }
+          });
+
+          view.showDropdownMenu();
+
+          var menuitem = view.getDOMNode().querySelector(".entry-settings-notifications");
+
+          expect(menuitem.textContent).eql("settings_menu_item_turnnotificationson");
         });
 
         it("should toggle mozLoop.doNotDisturb to true", function() {
@@ -1317,7 +1343,7 @@ describe("loop.panel", function() {
       var view = createTestComponent();
 
       var node = view.getDOMNode();
-      expect(node.querySelector(".room-entry h2").textContent).to.equal("Fake title");
+      expect(node.querySelector(".room-entry h2").textContent).to.equal("room_name_untitled_page");
     });
 
     describe("computeAdjustedTopPosition", function() {

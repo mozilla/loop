c67df45fc6580568aed9447ccdc5b953818e2d86
Manuel Casas Barrado <manuel.casasbarrado@gmail.com>
2016-04-26 17:26:46 -0700

    Bug 1261488 - Sidebar desktop side, part 2, r=Standard8

--- add-on/panels/js/sidebar.jsx
+++ add-on/panels/js/sidebar.jsx
@@ -3,250 +3,166 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var loop = loop || {};
-loop.conversation = (function(mozL10n) {
+
+loop.sidebar = (function() {
   "use strict";
-
-  var sharedMixins = loop.shared.mixins;
+console.info(loop);
+  var ROOM_STATES = loop.store.ROOM_STATES;
   var sharedActions = loop.shared.actions;
-  var FAILURE_DETAILS = loop.shared.utils.FAILURE_DETAILS;
+  var sharedUtils = loop.shared.utils;
+  var sharedMixins = loop.shared.mixins;
 
-  var DesktopRoomConversationView = loop.roomViews.DesktopRoomConversationView;
-  var FeedbackView = loop.feedbackViews.FeedbackView;
-  var RoomFailureView = loop.roomViews.RoomFailureView;
+  loop.config = loop.config || {};
+  loop.config.serverUrl = loop.config.serverUrl || "http://localhost:5000";
 
-  /**
-   * Master controller view for handling if incoming or outgoing calls are
-   * in progress, and hence, which view to display.
-   */
-  var AppControllerView = React.createClass({
-    mixins: [
-      Backbone.Events,
-      loop.store.StoreMixin("conversationAppStore"),
-      sharedMixins.DocumentTitleMixin,
-      sharedMixins.WindowCloseMixin
-    ],
+  var SidebarControllerView = React.createClass({
+    mixins: [sharedMixins.UrlHashChangeMixin,
+             sharedMixins.DocumentLocationMixin,
+             Backbone.Events],
 
     propTypes: {
-      cursorStore: React.PropTypes.instanceOf(loop.store.RemoteCursorStore).isRequired,
+      activeRoomStore: React.PropTypes.instanceOf(loop.store.ActiveRoomStore).isRequired,
       dispatcher: React.PropTypes.instanceOf(loop.Dispatcher).isRequired,
-      roomStore: React.PropTypes.instanceOf(loop.store.RoomStore)
-    },
-
-    componentWillMount: function() {
-      this.listenTo(this.props.cursorStore, "change:remoteCursorPosition",
-                    this._onRemoteCursorPositionChange);
-      this.listenTo(this.props.cursorStore, "change:remoteCursorClick",
-                    this._onRemoteCursorClick);
-    },
-
-    _onRemoteCursorPositionChange: function() {
-      loop.request("AddRemoteCursorOverlay",
-                  this.props.cursorStore.getStoreState("remoteCursorPosition"));
-    },
-
-    _onRemoteCursorClick: function() {
-      let click = this.props.cursorStore.getStoreState("remoteCursorClick");
-      // if the click is 'false', assume it is a storeState reset,
-      // so don't do anything
-      if (!click) {
-        return;
-      }
-
-      this.props.cursorStore.setStoreState({
-        remoteCursorClick: false
-      });
-
-      loop.request("ClickRemoteCursor", click);
+      standaloneAppStore: React.PropTypes.instanceOf(
+        loop.store.StandaloneAppStore).isRequired
     },
 
     getInitialState: function() {
-      return this.getStoreState();
+      return this.props.standaloneAppStore.getStoreState();
     },
 
-    _renderFeedbackForm: function() {
-      this.setTitle(mozL10n.get("conversation_has_ended"));
-
-      return (<FeedbackView
-        onAfterFeedbackReceived={this.closeWindow} />);
+    componentWillMount: function() {
+      this.listenTo(this.props.standaloneAppStore, "change", function() {
+        this.setState(this.props.standaloneAppStore.getStoreState());
+      }, this);
     },
 
-    /**
-     * We only show the feedback for once every 6 months, otherwise close
-     * the window.
-     */
-    handleCallTerminated: function() {
-      this.props.dispatcher.dispatch(new sharedActions.LeaveConversation());
+    componentWillUnmount: function() {
+      this.stopListening(this.props.standaloneAppStore);
+    },
+
+    onUrlHashChange: function() {
+      this.locationReload();
     },
 
     render: function() {
-      if (this.state.showFeedbackForm) {
-        return this._renderFeedbackForm();
-      }
-
       switch (this.state.windowType) {
         case "room": {
-          return (<DesktopRoomConversationView
-            chatWindowDetached={this.state.chatWindowDetached}
-            cursorStore={this.props.cursorStore}
-            dispatcher={this.props.dispatcher}
-            facebookEnabled={this.state.facebookEnabled}
-            onCallTerminated={this.handleCallTerminated}
-            roomStore={this.props.roomStore} />);
-        }
-        case "failed": {
-          return (<RoomFailureView
-            dispatcher={this.props.dispatcher}
-            failureReason={FAILURE_DETAILS.UNKNOWN} />);
+          return (
+             <SidebarView
+               activeRoomStore={this.props.activeRoomStore}
+               dispatcher={this.props.dispatcher} />
+          );
         }
         default: {
-          // If we don't have a windowType, we don't know what we are yet,
-          // so don't display anything.
-          return null;
+          // The state hasn't been initialised yet, so don't display
+          // anything to avoid flicker.
+          return (
+            <div>
+              <p>I'm a sidebar</p>
+            </div>
+          );
         }
       }
     }
   });
 
-  /**
-   * Conversation initialisation.
-   */
-  function init() {
-    // Obtain the windowId and pass it through
-    var locationHash = loop.shared.utils.locationData().hash;
-    var windowId;
+  var SidebarView = React.createClass({
+    propTypes: {
+      activeRoomStore: React.PropTypes.instanceOf(loop.store.ActiveRoomStore).isRequired,
+      dispatcher: React.PropTypes.instanceOf(loop.Dispatcher).isRequired
+    },
 
-    var hash = locationHash.match(/#(.*)/);
-    if (hash) {
-      windowId = hash[1];
+    getInitialState: function() {
+      var storeState = this.props.activeRoomStore.getStoreState();
+      return storeState;
+    },
+
+    leaveRoom: function() {
+      this.props.dispatcher.dispatch(new sharedActions.LeaveRoom());
+    },
+
+    /**
+     * Checks if current room is active.
+     *
+     * @return {Boolean}
+     */
+    _roomIsActive: function() {
+      return this.state.roomState === ROOM_STATES.JOINED ||
+             this.state.roomState === ROOM_STATES.SESSION_CONNECTED ||
+             this.state.roomState === ROOM_STATES.HAS_PARTICIPANTS;
+    },
+
+    render: function() {
+      return (
+        <loop.shared.toc.SidebarView
+          activeRoomStore={this.props.activeRoomStore}
+          audio={{ enabled: !this.state.audioMuted,
+                   visible: this._roomIsActive() }}
+          dispatcher={this.props.dispatcher}
+          isFirefox={true}
+          leaveRoom={this.leaveRoom}
+          video={{ enabled: !this.state.videoMuted,
+                   visible: this._roomIsActive() }} />
+      );
     }
+  });
 
-    var requests = [
-      ["GetAllConstants"],
-      ["GetAllStrings"],
-      ["GetLocale"],
-      ["GetLoopPref", "ot.guid"],
-      ["GetLoopPref", "feedback.periodSec"],
-      ["GetLoopPref", "feedback.dateLastSeenSec"],
-      ["GetLoopPref", "facebook.enabled"]
-    ];
-    var prefetch = [
-      ["GetConversationWindowData", windowId]
-    ];
-
-    return loop.requestMulti.apply(null, requests.concat(prefetch)).then(function(results) {
-      // `requestIdx` is keyed off the order of the `requests` and `prefetch`
-      // arrays. Be careful to update both when making changes.
-      var requestIdx = 0;
-      var constants = results[requestIdx];
-      // Do the initial L10n setup, we do this before anything
-      // else to ensure the L10n environment is setup correctly.
-      var stringBundle = results[++requestIdx];
-      var locale = results[++requestIdx];
-      mozL10n.initialize({
-        locale: locale,
-        getStrings: function(key) {
-          if (!(key in stringBundle)) {
-            console.error("No string found for key: ", key);
-            return "{ textContent: '' }";
-          }
-
-          return JSON.stringify({ textContent: stringBundle[key] });
-        }
-      });
-
-      // Plug in an alternate client ID mechanism, as localStorage and cookies
-      // don't work in the conversation window
-      var currGuid = results[++requestIdx];
-      window.OT.overrideGuidStorage({
-        get: function(callback) {
-          callback(null, currGuid);
-        },
-        set: function(guid, callback) {
-          // See nsIPrefBranch
-          var PREF_STRING = 32;
-          currGuid = guid;
-          loop.request("SetLoopPref", "ot.guid", guid, PREF_STRING);
-          callback(null);
-        }
-      });
-
-      var dispatcher = new loop.Dispatcher();
-      var sdkDriver = new loop.OTSdkDriver({
-        constants: constants,
-        isDesktop: true,
-        useDataChannels: true,
-        dispatcher: dispatcher,
-        sdk: OT
-      });
-
-      // expose for functional tests
-      loop.conversation._sdkDriver = sdkDriver;
-
-      // Create the stores.
-      var activeRoomStore = new loop.store.ActiveRoomStore(dispatcher, {
-        isDesktop: true,
-        sdkDriver: sdkDriver
-      });
-      var conversationAppStore = new loop.store.ConversationAppStore({
-        activeRoomStore: activeRoomStore,
-        dispatcher: dispatcher,
-        feedbackPeriod: results[++requestIdx],
-        feedbackTimestamp: results[++requestIdx],
-        facebookEnabled: results[++requestIdx]
-      });
-
-      prefetch.forEach(function(req) {
-        req.shift();
-        loop.storeRequest(req, results[++requestIdx]);
-      });
-
-      var roomStore = new loop.store.RoomStore(dispatcher, {
-        activeRoomStore: activeRoomStore,
-        constants: constants
-      });
-      var textChatStore = new loop.store.TextChatStore(dispatcher, {
-        sdkDriver: sdkDriver
-      });
-      var remoteCursorStore = new loop.store.RemoteCursorStore(dispatcher, {
-        sdkDriver: sdkDriver
-      });
-
-      loop.store.StoreMixin.register({
-        conversationAppStore: conversationAppStore,
-        remoteCursorStore: remoteCursorStore,
-        textChatStore: textChatStore
-      });
-
-      React.render(
-        <AppControllerView
-          cursorStore={remoteCursorStore}
-          dispatcher={dispatcher}
-          roomStore={roomStore} />, document.querySelector("#main"));
-
-      document.documentElement.setAttribute("lang", mozL10n.language.code);
-      document.documentElement.setAttribute("dir", mozL10n.language.direction);
-      document.body.setAttribute("platform", loop.shared.utils.getPlatform());
-
-      dispatcher.dispatch(new sharedActions.GetWindowData({
-        windowId: windowId
-      }));
-
-      loop.request("TelemetryAddValue", "LOOP_ACTIVITY_COUNTER", constants.LOOP_MAU_TYPE.OPEN_CONVERSATION);
+  function init() {
+    loop.StandaloneMozLoop({
+      baseServerUrl: loop.config.serverUrl
     });
+
+    var dispatcher = new loop.Dispatcher();
+    var sdkDriver = new loop.OTSdkDriver({
+      // For the standalone, always request data channels. If they aren't
+      // implemented on the client, there won't be a similar message to us, and
+      // we won't display the UI.
+      constants: {},
+      useDataChannels: true,
+      dispatcher: dispatcher,
+      sdk: OT
+    });
+
+    // Stores
+    var standaloneAppStore = new loop.store.StandaloneAppStore({
+      dispatcher: dispatcher,
+      sdk: OT
+    });
+
+    var activeRoomStore = new loop.store.ActiveRoomStore(dispatcher, {
+      sdkDriver: sdkDriver
+    });
+
+    var textChatStore = new loop.store.TextChatStore(dispatcher, {
+      sdkDriver: sdkDriver
+    });
+
+    loop.store.StoreMixin.register({
+      activeRoomStore: activeRoomStore,
+      textChatStore: textChatStore
+    });
+
+    window.addEventListener("unload", function() {
+      dispatcher.dispatch(new sharedActions.WindowUnload());
+    });
+
+    var locationData = sharedUtils.locationData();
+
+    dispatcher.dispatch(new sharedActions.ExtractTokenInfo({
+      windowPath: locationData.pathname,
+      windowHash: locationData.hash
+    }));
+
+    React.render(<SidebarControllerView
+                    activeRoomStore={activeRoomStore}
+                    dispatcher={dispatcher}
+                    standaloneAppStore={standaloneAppStore} />, document.querySelector("#main"));
   }
 
   return {
-    AppControllerView: AppControllerView,
-    init: init,
-
-    /**
-     * Exposed for the use of functional tests to be able to check
-     * metric-related execution as the call sequence progresses.
-     *
-     * @type loop.OTSdkDriver
-     */
-    _sdkDriver: null
+    init: init
   };
-})(document.mozL10n);
+})();
 
-document.addEventListener("DOMContentLoaded", loop.conversation.init);
+document.addEventListener("DOMContentLoaded", loop.sidebar.init);

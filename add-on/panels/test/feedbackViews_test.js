e23701026ff5d890e0f10b7c50107b1d55a8c002
David Critchley <dcritchley@mozilla.com>
2016-05-05 06:21:13 -0700

    Bug 1257266 - Change NPS survey link to dynamically add firefox hello version number to survey link. r=Standard8

--- add-on/panels/test/feedbackViews_test.js
+++ add-on/panels/test/feedbackViews_test.js
@@ -22,8 +22,10 @@ describe("loop.feedbackViews", function() {
   });
 
   describe("FeedbackView", function() {
-    var openURLStub, getLoopPrefStub, feedbackReceivedStub, view;
-    var fakeURL = "fake.form";
+    var openURLStub, getLoopPrefStub, feedbackReceivedStub, getAddonVersionStub;
+    var view;
+    var fakeURL = "fake.form?version=";
+    var addonVersion = "1.3.0";
 
     function mountTestComponent(props) {
       props = _.extend({
@@ -38,10 +40,12 @@ describe("loop.feedbackViews", function() {
       openURLStub = sandbox.stub();
       getLoopPrefStub = sandbox.stub();
       feedbackReceivedStub = sandbox.stub();
+      getAddonVersionStub = sandbox.stub();
 
       LoopMochaUtils.stubLoopRequest({
         OpenURL: openURLStub,
-        GetLoopPref: getLoopPrefStub
+        GetLoopPref: getLoopPrefStub,
+        GetAddonVersion: getAddonVersionStub
       });
     });
 
@@ -70,6 +74,7 @@ describe("loop.feedbackViews", function() {
 
     it("should open a new page to the feedback form", function() {
       getLoopPrefStub.withArgs("feedback.formURL").returns(fakeURL);
+      getAddonVersionStub.returns(addonVersion);
       view = mountTestComponent();
 
       TestUtils.Simulate.click(ReactDOM.findDOMNode(view.refs.feedbackFormBtn));
@@ -80,6 +85,7 @@ describe("loop.feedbackViews", function() {
 
     it("should fetch the feedback form URL from the prefs", function() {
       getLoopPrefStub.withArgs("feedback.formURL").returns(fakeURL);
+      getAddonVersionStub.returns(addonVersion);
       view = mountTestComponent();
 
       TestUtils.Simulate.click(ReactDOM.findDOMNode(view.refs.feedbackFormBtn));
@@ -88,7 +94,20 @@ describe("loop.feedbackViews", function() {
       sinon.assert.calledWithExactly(getLoopPrefStub, "feedback.formURL");
     });
 
+    it("should fetch the addon version", function() {
+      getLoopPrefStub.withArgs("feedback.formURL").returns(fakeURL);
+      getAddonVersionStub.returns(addonVersion);
+      view = mountTestComponent();
+
+      TestUtils.Simulate.click(ReactDOM.findDOMNode(view.refs.feedbackFormBtn));
+
+      sinon.assert.calledOnce(getAddonVersionStub);
+      sinon.assert.calledWithExactly(getAddonVersionStub);
+    });
+
     it("should close the window after opening the form", function() {
+      getLoopPrefStub.withArgs("feedback.formURL").returns(fakeURL);
+      getAddonVersionStub.returns(addonVersion);
       view = mountTestComponent();
 
       TestUtils.Simulate.click(ReactDOM.findDOMNode(view.refs.feedbackFormBtn));

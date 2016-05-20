9ac0563d0e5ccfb60f8c04eca79dd15b618526cc
Manuel Casas Barrado <manuel.casasbarrado@gmail.com>
2016-05-20 13:49:41 +0200

    Bug 1274546 - Initialize mozL10n on ToC startup. r=Mardak

--- add-on/panels/js/roomToc.jsx
+++ add-on/panels/js/roomToc.jsx
@@ -3,7 +3,8 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 var loop = loop || {};
 
-loop.roomToc = (function() {
+// XXX akita add tests
+loop.roomToc = (function(mozL10n) {
   "use strict";
   var tocViews = loop.shared.toc;
   var sharedActions = loop.shared.actions;
@@ -12,51 +13,67 @@ loop.roomToc = (function() {
   function init() {
     loop.shared.utils.getBoolPreference = function foo() {};
 
-    var dispatcher = new loop.Dispatcher();
-    var sdkDriver = new loop.OTSdkDriver({
-      // For the standalone, always request data channels. If they aren't
-      // implemented on the client, there won't be a similar message to us, and
-      // we won't display the UI.
-      constants: {},
-      useDataChannels: true,
-      dispatcher: dispatcher,
-      sdk: OT
+    var requests = [
+      ["GetAllConstants"],
+      ["GetAllStrings"],
+      ["GetLocale"]
+    ];
+
+    loop.requestMulti.apply(null, requests).then(function(results) {
+      let [constants, stringBundle, locale] = results;
+      mozL10n.initialize({
+        locale: locale,
+        getStrings: function(key) {
+          if (!(key in stringBundle)) {
+            console.error("No string found for key: ", key);
+            return JSON.stringify({ textContent: "foo" });
+          }
+
+          return JSON.stringify({ textContent: stringBundle[key] });
+        }
+      });
+
+      var dispatcher = new loop.Dispatcher();
+      var sdkDriver = new loop.OTSdkDriver({
+        // For the standalone, always request data channels. If they aren't
+        // implemented on the client, there won't be a similar message to us, and
+        // we won't display the UI.
+        constants,
+        useDataChannels: true,
+        dispatcher,
+        sdk: OT
+      });
+
+      var activeRoomStore = new loop.store.ActiveRoomStore(dispatcher, { sdkDriver });
+
+      var roomStore = new loop.store.RoomStore(dispatcher, { constants });
+
+      loop.store.StoreMixin.register({
+        activeRoomStore,
+        roomStore
+      });
+
+      window.addEventListener("unload", function() {
+        dispatcher.dispatch(new sharedActions.WindowUnload());
+      });
+
+      ReactDOM.render(<tocViews.TableOfContentView
+                        activeRoomStore={activeRoomStore}
+                        dispatcher={dispatcher}
+                        isScreenShareActive={false} />, document.querySelector("#main"));
+
+      var locationData = sharedUtils.locationData();
+      var hash = locationData.hash.match(/#(.*)/);
+
+      dispatcher.dispatch(new sharedActions.SetupWindowData({
+        roomToken: hash[1]
+      }));
     });
-
-    var activeRoomStore = new loop.store.ActiveRoomStore(dispatcher, {
-      sdkDriver: sdkDriver
-    });
-
-    // XXX akita: Get loop constants
-    var roomStore = new loop.store.RoomStore(dispatcher, {
-      constants: { CONSTANT: false }
-    });
-
-    loop.store.StoreMixin.register({
-      activeRoomStore: activeRoomStore,
-      roomStore: roomStore
-    });
-
-    window.addEventListener("unload", function() {
-      dispatcher.dispatch(new sharedActions.WindowUnload());
-    });
-
-    ReactDOM.render(<tocViews.TableOfContentView
-                      activeRoomStore={activeRoomStore}
-                      dispatcher={dispatcher}
-                      isScreenShareActive={false} />, document.querySelector("#main"));
-
-    var locationData = sharedUtils.locationData();
-    var hash = locationData.hash.match(/#(.*)/);
-
-    dispatcher.dispatch(new sharedActions.SetupWindowData({
-      roomToken: hash[1]
-    }));
   }
 
   return {
     init: init
   };
-})();
+})(document.mozL10n);
 
 document.addEventListener("DOMContentLoaded", loop.roomToc.init);
